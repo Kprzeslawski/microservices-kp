1e6a63f90ffdb0f4b23f07b30293bc8c8607aefe
Krzysztof <kprzeslawski01@gmail.com>
2024-03-28 22:29:24 +0100

    add base item register and item manipulation handler for creating instances of items

---
+++ character_service_module/src/main/java/com/przeslawskik/character_module/other/ItemsManipulationHandler.java
@@ -0,0 +1,47 @@
+package com.przeslawskik.character_module.other;
+
+import com.mongodb.lang.Nullable;
+import com.przeslawskik.character_module.documents.Hero;
+import com.przeslawskik.character_module.documents.Item;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class ItemsManipulationHandler {
+
+    public static Item createNewItemBasedOnRegister(
+            BaseItem template, Hero forWho
+    ) {
+        return createNewItemBasedOnRegister(template,forWho, 0.2);
+    }
+    public static Item createNewItemBasedOnRegister(
+            BaseItem template, Hero forWho,
+            @Nullable double statsVariation
+    ){
+        Random r = new Random();
+        double randomValue = 1. - statsVariation + statsVariation * 2 * r.nextDouble();
+        return Item.builder()
+                .hero(forWho)
+                .name(template.getName())
+                .stats(
+                        template
+                                .getAvg_stats()
+                                .entrySet()
+                                .stream()
+                                .collect(Collectors.toMap(
+                                        Map.Entry::getKey,
+                                        m -> ((int) Math.round(m.getValue() * randomValue))
+                                ))
+                )
+                .build();
+    }
+
+
+
+
+
+
+
+
+}
