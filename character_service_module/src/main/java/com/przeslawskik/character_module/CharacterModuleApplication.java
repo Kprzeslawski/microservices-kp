71e6d06a1b2bc2a816a479ad4804aa31c48c2656
Krzysztof <kprzeslawski01@gmail.com>
2024-04-03 14:06:03 +0200

    fix issues and proper connection

--- character_service_module/src/main/java/com/przeslawskik/character_module/CharacterModuleApplication.java
+++ character_service_module/src/main/java/com/przeslawskik/character_module/CharacterModuleApplication.java
@@ -2,9 +2,7 @@ package com.przeslawskik.character_module;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.*;
 @SpringBootApplication
-@EnableEurekaClient
 public class CharacterModuleApplication {
 
 	public static void main(String[] args) {
--- loc_and_mob_module/src/main/java/com/przeslawskik/loc_n_mob_module/LocationsAndMobModuleApplication.java
+++ loc_and_mob_module/src/main/java/com/przeslawskik/loc_n_mob_module/LocationsAndMobModuleApplication.java
@@ -2,10 +2,8 @@ package com.przeslawskik.loc_n_mob_module;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class LocationsAndMobModuleApplication {
     public static void main(String[] args) {
         SpringApplication.run(LocationsAndMobModuleApplication.class, args);
