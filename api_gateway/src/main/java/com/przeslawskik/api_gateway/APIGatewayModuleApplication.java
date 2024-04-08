43e5a3ecb76dbfbed94044cb55bfac115ac2e208
Krzysztof <kprzeslawski01@gmail.com>
2024-04-08 21:21:02 +0200

    add gateway main class and properties

---
+++ api_gateway/src/main/java/com/przeslawskik/api_gateway/APIGatewayModuleApplication.java
@@ -0,0 +1,11 @@
+package com.przeslawskik.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIGatewayModuleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(APIGatewayModuleApplication.class, args);
+    }
+}
