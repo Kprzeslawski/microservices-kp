de337196e4ac948fbac88bc599c5284d17b464c0
Krzysztof <kprzeslawski01@gmail.com>
2024-04-23 22:50:28 +0200

    added register map for better access

--- character_service_module/src/main/java/com/przeslawskik/character_module/ResourcesRegister/LocationRegister.java
+++ character_service_module/src/main/java/com/przeslawskik/character_module/ResourcesRegister/LocationRegister.java
@@ -1,14 +1,21 @@
 package com.przeslawskik.character_module.ResourcesRegister;
 
+import com.przeslawskik.character_module.ResourcesRegister.entities.EnemyEntity;
 import com.przeslawskik.character_module.ResourcesRegister.entities.ItemEntity;
 import com.przeslawskik.character_module.ResourcesRegister.entities.LocationEntity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LocationRegister {
     public static final LocationEntity FOREST =
             new LocationEntity("forest", 1, new ArrayList<>(){{
                 add(EnemiesRegister.ZOMBIE);
             }});
+    public static final Map<String, LocationEntity> register = new HashMap<>(){{
+        put(FOREST.getName(),FOREST);
+    }};
+
 }
