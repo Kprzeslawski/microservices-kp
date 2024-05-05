5070c0a7d36a2393bc2447d15434189b21428f69
Krzysztof <kprzeslawski01@gmail.com>
2024-05-05 23:00:59 +0200

    added enemy - guardian

--- character_service_module/src/main/java/com/przeslawskik/character_module/ResourcesRegister/EnemiesRegister.java
+++ character_service_module/src/main/java/com/przeslawskik/character_module/ResourcesRegister/EnemiesRegister.java
@@ -13,8 +13,23 @@ public class EnemiesRegister {
             new ArrayList<>(){{
                 add(new EnemyEntity.LootTableElem(ItemsRegister.WOODEN_SWORD,1.));
             }});
+
+    public static final EnemyEntity GUARDIAN = new EnemyEntity("guardian",
+            Stats.builder().health(60).attack_dmg(5).armor(3).build(),10,12, 15, 20,
+            new ArrayList<>(){{
+                add(new EnemyEntity.LootTableElem(ItemsRegister.IRON_SWORD,0.1));
+                add(new EnemyEntity.LootTableElem(ItemsRegister.IRON_HELMET,0.1));
+                add(new EnemyEntity.LootTableElem(ItemsRegister.IRON_CHESTPLATE,0.1));
+                add(new EnemyEntity.LootTableElem(ItemsRegister.IRON_LEGGINGS,0.1));
+                add(new EnemyEntity.LootTableElem(ItemsRegister.IRON_BOOTS,0.1));
+            }});
+
+
+
+
     public static final Map<String,EnemyEntity> register = new HashMap<>(){{
         put(ZOMBIE.getName(),ZOMBIE);
+        put(GUARDIAN.getName(),GUARDIAN);
     }};
 
 
