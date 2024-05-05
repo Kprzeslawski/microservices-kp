fbeb8599e89a007ff70c669b7adb5c68d3c8026b
Krzysztof <kprzeslawski01@gmail.com>
2024-05-05 22:53:00 +0200

    added items - leather

--- character_service_module/src/main/java/com/przeslawskik/character_module/ResourcesRegister/ItemsRegister.java
+++ character_service_module/src/main/java/com/przeslawskik/character_module/ResourcesRegister/ItemsRegister.java
@@ -20,10 +20,36 @@ public class ItemsRegister {
             new ItemEntity("iron sword", SlotEnum.HAND ,new HashMap<>(){{
                 put(StatsEnum.ATTACK_DAMAGE,3);put(StatsEnum.ARMOR,2);
             }});
+
+    public static final ItemEntity LEATHER_BOOTS =
+            new ItemEntity("leather boots", SlotEnum.BOOTS ,new HashMap<>(){{
+                put(StatsEnum.HEALTH,4);put(StatsEnum.ARMOR,2);
+            }});
+
+    public static final ItemEntity LEATHER_CHESTPLATE =
+            new ItemEntity("leather chestplate", SlotEnum.CHEST ,new HashMap<>(){{
+                put(StatsEnum.HEALTH,4);put(StatsEnum.ARMOR,2);
+            }});
+
+    public static final ItemEntity LEATHER_LEGGINGS =
+            new ItemEntity("leather leggings", SlotEnum.LEGGINGS ,new HashMap<>(){{
+                put(StatsEnum.HEALTH,4);put(StatsEnum.ARMOR,2);
+            }});
+
+    public static final ItemEntity LEATHER_HELMET =
+            new ItemEntity("leather helmet", SlotEnum.HELMET ,new HashMap<>(){{
+                put(StatsEnum.HEALTH,4);put(StatsEnum.ARMOR,2);
+            }});
+
     public static final Map<String, ItemEntity> register = new HashMap<>(){{
         put(WOODEN_SWORD.getName(),WOODEN_SWORD);
         put(STONE_SWORD.getName(),STONE_SWORD);
         put(IRON_SWORD.getName(),IRON_SWORD);
+
+        put(LEATHER_BOOTS.getName(),LEATHER_BOOTS);
+        put(LEATHER_CHESTPLATE.getName(),LEATHER_CHESTPLATE);
+        put(LEATHER_LEGGINGS.getName(),LEATHER_LEGGINGS);
+        put(LEATHER_HELMET.getName(),LEATHER_HELMET);
     }};
 
 }
